0cdf48d39338421a86835c36adcbb6d808fe003f
shahaf noked <shahafnoked@gmail.com>
2024-09-15 17:24:17 +0300

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+function App() {
+  return (
+    <>
+     <h1>React app</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+
+import './assets/styles/main.scss'
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+    <App />
+  // </StrictMode>,
+)
